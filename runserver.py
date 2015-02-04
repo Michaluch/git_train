39f7a1891f58dfdf3201bf4bd338190ff6154505
Darkness <siteofmichal@gmail.com>
2015-02-04 13:38:09 +0200

    add error handler, templates for base.html and error.html

--- runserver.py
+++ runserver.py
@@ -10,5 +10,10 @@ app.route('/')
 def index():
     return "Hello World!"
 
+app.errorhandler(404)
+def error404(error):
+    return render_template('error.html'), 404
+
+
 if __name__ = "__main__":
     app.run()
