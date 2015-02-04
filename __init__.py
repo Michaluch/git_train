15c238368c69cb14dd7bdb3b34950bc8bd9c18fe
Mykhailo Omelchuk <siteofmichal@gmail.com>
2015-02-04 13:09:25 +0200

    Add first files into project

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 02:54:37 2014
+
+@author: localadmin
+"""
+
+MONGO_CREDENTIALS = dict(
+    db = 'mikeblog', 
+    host = 'paulo.mongohq.com', 
+    port = 10067, 
+    user = 'michaluch', 
+    password = '576michaluch'
+)
