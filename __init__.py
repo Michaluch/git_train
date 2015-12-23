a12919d80204ed637e45210f5accc8f35c262b6a
user <user@rvtempmacmini06.local>
2015-12-23 15:38:54 +0200

    add something else

--- __init__.py
+++ __init__.py
@@ -5,10 +5,11 @@ Created on Wed Jan 15 02:54:37 2014
 @author: localadmin
 """
 
-MONGO_CREDENTIALS = dict(
-    db = 'mikeblog', 
-    host = 'paulo.mongohq.com', 
-    port = 10067, 
-    user = 'michaluch', 
+
+:qMONGO_CREDENTIALS = dict(
+    db = 'mikeblog',
+    host = 'paulo.mongohq.com',
+    port = 10067,
+    user = 'michaluch',
     password = '576michaluch'
 )
